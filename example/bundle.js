394bfc083d30a2c926eb8aa1c1c9d116d56e9f31
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2013-07-15 18:00:58 -0400

    upgrades lodash

--- example/bundle.js
+++ example/bundle.js
@@ -1,6 +1,6 @@
 require=(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({"./slider":[function(require,module,exports){
-module.exports=require('FiQ96f');
-},{}],"FiQ96f":[function(require,module,exports){
+module.exports=require('FLnI3w');
+},{}],"FLnI3w":[function(require,module,exports){
 var Backbone = require('backbone')
   , _ = require('lodash')
 
